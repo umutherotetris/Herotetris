c1b41f6c04207378e7692ab24dd52dbe2695cc99
umutherotetris <umutx123@gmail.com>
2026-06-19 11:50:00 +0300

    Update tavla.js

--- js/games/tavla.js
+++ js/games/tavla.js
@@ -1928,15 +1928,6 @@ function checkerPixel(idx, color, side){
 }
 
 // ════════════ YARDIMCI ÇİZİM ════════════
-function __roundRect(ctx, x, y, w, h, r){
-  ctx.beginPath();
-  ctx.moveTo(x+r, y);
-  ctx.arcTo(x+w, y, x+w, y+h, r);
-  ctx.arcTo(x+w, y+h, x, y+h, r);
-  ctx.arcTo(x, y+h, x, y, r);
-  ctx.arcTo(x, y, x+w, y, r);
-  ctx.closePath();
-}
 function star(ctx, cx, cy, spikes, outer, inner){
   ctx.beginPath();
   for(let i=0;i<spikes*2;i++){
